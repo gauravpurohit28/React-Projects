dd6ff8e8b89171bcebbee9750500180d6697fa55
Gaurav Purohit <grajpurohittt@gmail.com>
2025-07-03 23:27:57 +0530

    initial commit

---
+++ 3d-portfolio/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <header className="absolute w-full z-50 transition-all duration-300">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 md:h-20">
+          
+        </div>
+    </header>
+  )
+}
+
+export default Header

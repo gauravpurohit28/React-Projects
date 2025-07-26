6bdbef8f0576e83cf0f52eb0185e521a51f68d90
Gaurav Purohit <grajpurohittt@gmail.com>
2025-07-27 00:37:20 +0530

    new project

---
+++ Docs/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className='relative w-full h-screen bg-zinc-800'>
+      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-900 text-[13vw] leading-none tracking-tighter'>DOCS</h1>
+    </div>
+  )
+}
+
+export default App
